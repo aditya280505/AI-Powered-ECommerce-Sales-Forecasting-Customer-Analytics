2443da71a1f75d6f4501cf039c65be7187d172b2
ADITYA BORGAONKAR <borgaonkaraditya1@gmail.com>
2026-06-16 16:39:08 +0530

    Create streamlit_app.py

---
+++ app/streamlit_app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="E-Commerce Analytics Dashboard",
+    page_icon="📊",
+    layout="wide"
+)
+
+st.title("📊 AI-Powered E-Commerce Sales Forecasting & Customer Analytics Platform")
+
+st.markdown("""
+Welcome to the dashboard.
+
+Use the sidebar to navigate between pages.
+""")
+
+#Run : streamlit run app/streamlit_app.py
